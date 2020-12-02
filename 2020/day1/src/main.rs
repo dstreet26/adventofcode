d6ff9fdbdb4ac83c24ea9db442e2620345b764c5
David Streeter <dcstreeter@pbs.org>
2020-12-02 07:33:29 -0500

    using "combinations" instead of nesting for-loops.

--- 2020/day1/src/main.rs
+++ 2020/day1/src/main.rs
@@ -1,43 +1,20 @@
+use itertools::Itertools;
 use std::fs;
-
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Couldn't read input file :(");
-    let list: Vec<&str> = contents.split("\n").collect();
-    // let theMatch = getMatch(list);
-    // let (a, b) = getMatch(list);
-    let (a, b, c) = getMatch3(list);
-    // let (println!("{:?}", theMatch);
-    println!("{}", a);
-    println!("{}", b);
-    println!("{}", c);
-}
 
-fn getMatch(list: Vec<&str>) -> (i32, i32) {
-    for i in &list {
-        for j in &list {
-            let a = i.parse::<i32>().unwrap();
-            let b = j.parse::<i32>().unwrap();
+    let combs: Vec<Vec<i32>> = contents
+        .split("\n")
+        .map(|x| x.parse::<i32>().unwrap())
+        .combinations(3)
+        .collect();
 
-            if (a + b == 2020) {
-                return (a, b);
-            }
+    for i in combs {
+        let sum: i32 = i.iter().sum();
+        if sum == 2020 {
+            let product: i32 = i.iter().product();
+            println!("{:?}", i);
+            println!("{:?}", product);
         }
     }
-    (0, 0)
-}
-fn getMatch3(list: Vec<&str>) -> (i32, i32, i32) {
-    for i in &list {
-        for j in &list {
-            for k in &list {
-                let a = i.parse::<i32>().unwrap();
-                let b = j.parse::<i32>().unwrap();
-                let c = k.parse::<i32>().unwrap();
-
-                if (a + b + c == 2020) {
-                    return (a, b, c);
-                }
-            }
-        }
-    }
-    (0, 0, 0)
 }
