f51b3b728d804b5de3f81bc251805cab8fa559c6
David Streeter <dcstreeter@pbs.org>
2020-12-02 08:37:03 -0500

    day2 part2 dirty

--- 2020/day2/src/main.rs
+++ 2020/day2/src/main.rs
@@ -1,4 +1,4 @@
-use itertools::Itertools;
+// use itertools::Itertools;
 use std::fs;
 
 fn main() {
@@ -9,14 +9,16 @@ fn main() {
     for i in &list {
         let list2: Vec<&str> = i.split(": ").collect();
         let list3: Vec<&str> = list2[0].split(" ").collect();
-        let letter = list3[1];
+        let letter = list3[1].chars().nth(0).unwrap();
         let list4: Vec<&str> = list3[0].split("-").collect();
-        let min = list4[0].parse().unwrap();
-        let max = list4[1].parse().unwrap();
-        let string = list2[1];
-        let countOfNumber = string.split("").filter(|x| x == &letter).count();
-        let pass = countOfNumber <= max && countOfNumber >= min;
-        if (pass) {
+        let position1: usize = list4[0].parse().unwrap();
+        let position2: usize = list4[1].parse().unwrap();
+        let the_string = list2[1];
+        let position1_contains = the_string.chars().nth(position1 - 1).unwrap() == letter;
+        let position2_contains = the_string.chars().nth(position2 - 1).unwrap() == letter;
+        let both = position1_contains && position2_contains;
+        let pass = !both && (position1_contains || position2_contains);
+        if pass {
             count = count + 1
         }
     }
