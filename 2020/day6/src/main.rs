087b0c5fbe32c3e9b81a8576db5d70078c75bd32
David Streeter <dcstreeter@pbs.org>
2020-12-06 15:16:14 -0500

    day6 part2

--- 2020/day6/src/main.rs
+++ 2020/day6/src/main.rs
@@ -4,14 +4,23 @@ use std::fs;
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Couldn't read input file :(");
     let list: Vec<Vec<&str>> = contents.split("\n\n").map(|x: &str| x.split("\n").collect()).collect();
-
     let mut count = 0;
-
-    for i in &list {
-        println!("i: {:?}", i);
-        let unique_chars = i.iter().flat_map(|s| s.chars()).unique().collect::<Vec<char>>().len();
-        println!("unique_chars: {:?}", unique_chars);
-        count = count + unique_chars;
+    for group in &list {
+        let chars_iter = group.iter().flat_map(|s| s.chars());
+        let chars_vec : Vec<char> = chars_iter.clone().collect();
+        let unique_votes : Vec<char> = chars_iter.unique().collect();
+        let group_count = group.len();
+        for unique_vote in &unique_votes {
+                let mut this_count = 0;
+                for vote in &chars_vec {
+                    if vote == unique_vote {
+                        this_count = this_count + 1;
+                    }
+                }
+                if this_count >= group_count {
+                    count = count + 1;
+                }
+            }
     }
     println!("count: {}", count);
 }
