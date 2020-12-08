21bd6fdf954db99ad2ec0a4273be4526751a47c2
David Streeter <dcstreeter@pbs.org>
2020-12-08 13:12:31 -0500

    day7 part2

--- 2020/day7/src/main.rs
+++ 2020/day7/src/main.rs
@@ -1,15 +1,13 @@
-// use itertools::Itertools;
 use std::fs;
 
 fn main() {
     let contents = fs::read_to_string("input.txt").expect("Couldn't read input file :(");
     let list: Vec<&str> = contents.split("\n").collect();
     let mut count = 0;
-    let mut paths = vec!["shiny gold".to_string()];
-    let mut paths2 = vec!["shiny gold".to_string()];
+    let mut paths = vec![("shiny gold".to_string(), 1)];
     while paths.len() > 0 {
-        let target = paths.pop().unwrap();
-        println!("target: {:?}", target);
+        let target2 = paths.pop().unwrap();
+        let (target, target_multiplier) = target2;
         for i in &list {
             let remove_period = i.replace(".", "");
             let left_right: Vec<&str> = remove_period.split("contain").map(|x| x.trim()).collect();
@@ -19,20 +17,16 @@ fn main() {
                 continue;
             }
             let right_split: Vec<&str> = right.split(",").map(|x| x.trim()).collect();
-            for innerbag in right_split {
-                let remove_bags = innerbag.replace(" bags", "").replace(" bag", "");
-                let split1: Vec<&str> = remove_bags.split(" ").collect();
-                let digit = split1[0].parse().unwrap_or(0);
-                let inner_color: String = split1[1..].join(" ");
-                if inner_color == target {
-                    println!("add: {:?}", left);
-                    if !paths2.contains(&left.clone()) {
-                        count = count + 1;
-                        paths.push(left.clone());
-                    }
-                    paths2.push(left.clone());
-                    break;
+            if left == target {
+                for innerbag in right_split {
+                    let remove_bags = innerbag.replace(" bags", "").replace(" bag", "");
+                    let split1: Vec<&str> = remove_bags.split(" ").collect();
+                    let digit = split1[0].parse().unwrap_or(0);
+                    let inner_color: String = split1[1..].join(" ");
+                    count = count + (digit * target_multiplier);
+                    paths.push((inner_color, (digit * target_multiplier)));
                 }
+                break;
             }
         }
     }
