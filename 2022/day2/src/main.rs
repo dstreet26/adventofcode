90e7a4b61339f3eb7a5d7a1a5f11d875e4fb76ab
David Streeter <dstreet26@gmail.com>
2022-12-05 14:11:33 -0500

    day2 part2

--- 2022/day2/src/main.rs
+++ 2022/day2/src/main.rs
@@ -16,90 +16,102 @@
 // Y means player2 needs to win
 // Z means player2 needs to draw
 
-
 use std::fs;
 fn main() {
-    let contents = fs::read_to_string("input0.txt").expect("Couldn't read input file :(");
+    let contents = fs::read_to_string("input.txt").expect("Couldn't read input file :(");
 
-    // turn the input into a list of tuples
+    let mut contents_list = Vec::new();
+    for line in contents.lines() {
+        let mut line_list = Vec::new();
+        for c in line.chars() {
+            if c != ' ' {
+                line_list.push(c);
+            }
+        }
+        contents_list.push(line_list);
+    }
 
-    let data = contents
-        .lines()
-        .map(|line| {
-            let mut iter = line.split_whitespace();
-            let a = iter.next().unwrap();
-            let b = iter.next().unwrap();
-            (a, b)
-        })
-        .collect::<Vec<_>>();
-
-    let part1a = part1(data.clone());
+    let part1a = part1(&contents_list);
 
     println!("part 1: {}", part1a);
 
-    // we can re-use the part1 function to do part2
-    // maybe we can just pre-process the data to make it look like part1
-    let newdata = data
-        .iter()
-        .map(|(a, b)| {
-            // let newa = match a {
-            //     // &"A" => "Y",
-            //     &"A" => asdf("A", b),
-            //     &"B" => "X",
-            //     &"C" => "Z",
-            //     _ => panic!("invalid input"),
-            // };
-            // (newa, b)
-            asdf(a, b)
-            
-        })
-        .collect::<Vec<_>>();
-    
-    let part2 = part1(newdata);
+    let mut contents_list2 = Vec::new();
+    for line in contents_list {
+        let mut line_list = Vec::new();
+        let a = line[0];
+        line_list.push(a);
+        let b = line[1];
 
-    println!("part 2: {}", part2);
-}
+        match a {
+            'A' => {
+                if b == 'X' {
+                    line_list.push('Z');
+                } else if b == 'Y' {
+                    line_list.push('X');
+                } else if b == 'Z' {
+                    line_list.push('Y');
+                }
+            }
+            'B' => {
+                if b == 'X' {
+                    line_list.push('X');
+                } else if b == 'Y' {
+                    line_list.push('Y');
+                } else if b == 'Z' {
+                    line_list.push('Z');
+                }
+            }
+            'C' => {
+                if b == 'X' {
+                    line_list.push('Y');
+                } else if b == 'Y' {
+                    line_list.push('Z');
+                } else if b == 'Z' {
+                    line_list.push('X');
+                }
+            }
+            _ => {}
+        }
 
-fn asdf(a: &str, b: &str) -> bool {
-    match (a, b) {
-        ("A", "Y") => true,
-        ("B", "X") => true,
-        ("C", "Z") => true,
-        _ => false,
+        contents_list2.push(line_list);
     }
+
+    println!("part 2: {}", part1(&contents_list2));
 }
 
-fn part1(input: Vec<(&str, &str)>) -> i32 {
+fn part1(input: &Vec<Vec<char>>) -> usize {
     let mut score = 0;
-    for (a, b) in input {
-        if b == "X" {
+    for line in input {
+        let a = line[0];
+        let b = line[1];
+        if b == 'X' {
             score += 1;
-        } else if b == "Y" {
+        } else if b == 'Y' {
             score += 2;
-        } else if b == "Z" {
+        } else if b == 'Z' {
             score += 3;
         }
 
-        if a == "A" {
-            if b == "X" {
+        if a == 'A' {
+            if b == 'X' {
                 score += 3;
-            } else if b == "Y" {
+            } else if b == 'Y' {
                 score += 6;
             } else {
                 score += 0;
             }
-        } else if a == "B" {
-            if b == "X" {
+        } else if a == 'B' {
+            if b == 'X' {
                 score += 0;
-            } else if b == "Y" {
+            } else if b == 'Y' {
                 score += 3;
             } else {
                 score += 6;
             }
         } else {
-            if b == "X" {
+            if b == 'X' {
                 score += 6;
-            } else if b == "Y" {
+            } else if b == 'Y' {
                 score += 0;
             } else {
                 score += 3;
@@ -108,5 +120,4 @@ fn part1(input: Vec<(&str, &str)>) -> i32 {
     }
     score
 }
-
 
